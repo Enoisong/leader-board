f80c781801b03fe329771bfa02e12eabc03c955b
Enoisong <enobongitiaba988@gmail.com>
2023-05-16 01:49:27 -0700

    fix linters

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,16 +1,16 @@
 const renderScores = (list) => {
-    const container = document.querySelector('.scores ul');
-    container.innerHTML = '';
+  const container = document.querySelector('.scores ul');
+  container.innerHTML = '';
 
-    if (!list.length) {
-        container.innerHTML = '<li>NO SCORES YET</li>';
-        return;        
-    }
+  if (!list.length) {
+    container.innerHTML = '<li>NO SCORES YET</li>';
+    return;
+  }
 
-    list
+  list
     .sort((a, b) => b.score - a.score)
     .forEach((score) => {
-        container.innerHTML += `<li>${score.user} : ${score.score}</li>`;
+      container.innerHTML += `<li>${score.user} : ${score.score}</li>`;
     });
 };
 
